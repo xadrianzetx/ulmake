4b0e1974598b5d284d199fc7389ac1743adb9199
xadrianzetx <xadrianzetx@gmail.com>
2021-11-09 20:35:20 +0100

    Post split iso size detection

--- src/game/iso.rs
+++ src/game/iso.rs
@@ -9,6 +9,19 @@ use regex::Regex;
 
 const SYSTEM_CNF_PATH: &str = "/SYSTEM.CNF";
 
+fn list_game_chunks(path: &Path, crc_name: &str) -> Result<Vec<String>> {
+    let chunks = fs::read_dir(path)?
+        .map(|res| res.unwrap().file_name().into_string().unwrap())
+        .filter(|n| n.contains(crc_name))
+        .collect::<Vec<_>>();
+
+    if chunks.len() == 0 {
+        return Err(Error::from(ErrorKind::NotFound));
+    }
+
+    Ok(chunks)
+}
+
 pub fn get_serial_from_iso(path: &Path) -> Result<String> {
     let isofile = File::open(path)?;
     let iso = match ISO9660::new(isofile) {
@@ -38,4 +51,17 @@ pub fn get_serial_from_iso(path: &Path) -> Result<String> {
 pub fn get_size_from_iso(isopath: &Path) -> Result<u64> {
     let metadata = fs::metadata(isopath)?;
     Ok(metadata.len())
+}
+
+pub fn get_size_from_chunks(dir: &Path, crc_name: &str) -> Result<u64> {
+    let mut total_size = 0;
+    let chunk_names = list_game_chunks(dir, crc_name)?;
+
+    for chunk_name in chunk_names.iter() {
+        let filepath = Path::new(dir).join(chunk_name);
+        let metadata = fs::metadata(filepath)?;
+        total_size += metadata.len();
+    }
+
+    Ok(total_size)
 }
