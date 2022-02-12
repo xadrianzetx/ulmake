03b45c6e8453fa62e9a3d7ca26d98007c9755d74
xadrianzetx <xadrianzetx@gmail.com>
2022-02-12 17:14:23 +0100

    Add chunk behavior for `ISOChunk` and `GameChunk`

--- src/game/iso.rs
+++ src/game/iso.rs
@@ -24,59 +24,80 @@ pub trait Chunk {
     fn count(&self) -> Result<u8>;
 }
 
-pub fn get_serial_from_iso(path: &Path) -> Result<String> {
-    let isofile = File::open(path)?;
-    let iso = match ISO9660::new(isofile) {
-        Ok(image) => image,
-        Err(_) => return Err(Error::from(ErrorKind::InvalidData)),
-    };
+impl Chunk for ISOChunk {
+    fn get_serial(&self) -> Result<String> {
+        let isofile = File::open(&self.path)?;
+        let iso = match ISO9660::new(isofile) {
+            Ok(image) => image,
+            Err(_) => return Err(Error::from(ErrorKind::InvalidData)),
+        };
 
-    match iso.open(SYSTEM_CNF_PATH).unwrap() {
-        Some(DirectoryEntry::File(file)) => {
-            let mut buffer = String::new();
-            file.read().read_to_string(&mut buffer)?;
+        match iso.open(SYSTEM_CNF_PATH).unwrap() {
+            Some(DirectoryEntry::File(file)) => {
+                let mut buffer = String::new();
+                file.read().read_to_string(&mut buffer)?;
 
-            // first line of SYSTEM.CNF should go like:
-            // BOOT2 = cdrom0:\SLXS_XXX.XX;1
-            // and we are fetching SLXS_XXX.XX from it
-            let re = Regex::new("([^:\\\\;]+)(:?;1)?$").unwrap();
-            let boot_path = buffer.lines().next().unwrap();
-            let boot_file = re.captures(boot_path).unwrap();
+                // first line of SYSTEM.CNF should go like:
+                // BOOT2 = cdrom0:\SLXS_XXX.XX;1
+                // and we are fetching SLXS_XXX.XX from it
+                let re = Regex::new("([^:\\\\;]+)(:?;1)?$").unwrap();
+                let boot_path = buffer.lines().next().unwrap();
+                let boot_file = re.captures(boot_path).unwrap();
 
-            let serial = boot_file.get(1).unwrap().as_str();
-            Ok(String::from(serial))
+                let serial = boot_file.get(1).unwrap().as_str();
+                Ok(String::from(serial))
+            }
+            _ => Err(Error::from(ErrorKind::NotFound)),
         }
-        _ => Err(Error::from(ErrorKind::NotFound)),
+    }
+
+    fn get_size(&self) -> Result<u64> {
+        let metadata = fs::metadata(&self.path)?;
+        Ok(metadata.len())
+    }
+
+    fn count(&self) -> Result<u8> {
+        Ok(0)
     }
 }
 
-pub fn get_serial_from_chunks(dir: &Path, crc_name: &str) -> Result<String> {
-    let chunks = list_game_chunks(dir, crc_name)?;
-    let chunk_name = chunks[0].to_owned();
-    let segments: Vec<&str> = chunk_name.split('.').collect();
-    let serial = format!("{}.{}", segments[2], segments[3]);
-    Ok(serial)
-}
-
-pub fn get_size_from_iso(isopath: &Path) -> Result<u64> {
-    let metadata = fs::metadata(isopath)?;
-    Ok(metadata.len())
-}
-
-pub fn get_size_from_chunks(dir: &Path, crc_name: &str) -> Result<u64> {
-    let mut total_size = 0;
-    let chunk_names = list_game_chunks(dir, crc_name)?;
-
-    for chunk_name in chunk_names.iter() {
-        let filepath = Path::new(dir).join(chunk_name);
-        let metadata = fs::metadata(filepath)?;
-        total_size += metadata.len();
+impl Chunk for GameChunk {
+    fn get_serial(&self) -> Result<String> {
+        let chunks = list_game_chunks(&self.path, &self.crc_name)?;
+        let chunk_name = chunks[0].to_owned();
+        let segments: Vec<&str> = chunk_name.split('.').collect();
+        let serial = format!("{}.{}", segments[2], segments[3]);
+        Ok(serial)
     }
 
-    Ok(total_size)
+    fn get_size(&self) -> Result<u64> {
+        let mut total_size = 0;
+        let chunk_names = list_game_chunks(&self.path, &self.crc_name)?;
+
+        for chunk_name in chunk_names.iter() {
+            let filepath = Path::new(&self.path).join(chunk_name);
+            let metadata = fs::metadata(filepath)?;
+            total_size += metadata.len();
+        }
+
+        Ok(total_size)
+    }
+
+    fn count(&self) -> Result<u8> {
+        let chunks = list_game_chunks(&self.path, &self.crc_name)?;
+        Ok(chunks.len() as u8)
+    }
 }
 
-pub fn count_chunks(dir: &Path, crc_name: &str) -> Result<u8> {
-    let chunks = list_game_chunks(dir, crc_name)?;
-    Ok(chunks.len() as u8)
+fn list_game_chunks(path: &Path, crc_name: &str) -> Result<Vec<String>> {
+    let chunks = fs::read_dir(path)?
+        .map(|res| res.unwrap().file_name().into_string().unwrap())
+        .filter(|n| n.contains(crc_name))
+        .collect::<Vec<_>>();
+
+    if chunks.is_empty() {
+        return Err(Error::from(ErrorKind::NotFound));
+    }
+
+    Ok(chunks)
 }
